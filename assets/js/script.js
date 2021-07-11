d4720a40b37a84b58ec3155b30e6c5811dbd7ff2
Harborth, Allen <allenharborth9835@gmail.com>
2021-07-10 22:03:48 -0500

    add user inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,14 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#task-to-do");
 
 var createTaskHandler = function(event){
-
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name-'task-type']").value;
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "this is a new task."
-    tasksToDoEl.appendChild(listItemEl)
-}
+    listItemEl.textContent = taskNameInput;
+    tasksToDoEl.appendChild(listItemEl);
+};
 
 formEl.addEventListener("submit", createTaskHandler);
